d5c0c2240de4f98d5f7653fd1f53d3f39fc1a2a7
Pivotal <pcf-redis@pivotal.io>
2018-12-17 14:51:40 +0000

    Changed application path

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p $GOPATH/src/github.com/totherme/trivial/hello
-cp -a . $GOPATH/src/github.com/totherme/trivial/hello
+mkdir -p $GOPATH/src/github.com/totherme
+cp -a . $GOPATH/src/github.com/totherme/
 go test ./...
