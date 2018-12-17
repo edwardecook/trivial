cb205431146db8873140d4943eb5744c4c4a1a42
Pivotal <pcf-redis@pivotal.io>
2018-12-17 15:51:51 +0000

    Added commands to move and run script into the script workind directory

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cf api $CF_API --skip-ssl-validation
-cf auth $CF_USER $CF_USER_PASSWORD
-cf target -o $CF_ORG -s $CF_SPACE
-cf push -f manifest.yml
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+pushd $SCRIPT_DIR
+  cf api $CF_API --skip-ssl-validation
+  cf auth $CF_USER $CF_USER_PASSWORD
+  cf target -o $CF_ORG -s $CF_SPACE
+  cf push -f manifest.yml
+popd
